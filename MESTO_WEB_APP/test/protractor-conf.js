4a9d1f0f3fd7e3ebda37287b496bd55f2242ea27
Jonathan Lefebvre <jonathan-lefebvreGitHub@outlook.com>
2015-06-04 12:43:32 -0400

    remove test comment that should not be comment

--- MESTO_WEB_APP/test/protractor-conf.js
+++ MESTO_WEB_APP/test/protractor-conf.js
@@ -1,10 +1,10 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     framework: 'jasmine2',
-    specs: [/*
-            'e2e/loginButton_test.js',*/
+    specs: [
+            'e2e/loginButton_test.js',
             'e2e/sites_test.js',
-            /*'e2e/rooms_test.js',
+            'e2e/rooms_test.js',
             'e2e/equipments_test.js',
             'e2e/userRoles_test.js',
             'e2e/users_test.js',
@@ -13,7 +13,7 @@ exports.config = {
             'e2e/securityUserRole_test.js',
             'e2e/securityEquipement_test.js',
             'e2e/securityRoom_test.js',
-            'e2e/securitySite_test.js'*/
+            'e2e/securitySite_test.js'
             ],
     //capabilities: [{browserName: 'IE'/*, browserName:'chrome'*/}] // TODO: make working IE Selenium plugins
     capabilities: {browserName: 'chrome'}
